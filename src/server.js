eb5f97cbab8e0491ad61ea2042dc4488bae010f6
b1naryb0t <b1naryb0t0000@gmail.com>
2025-11-23 01:29:18 -0500

    Import app as server entry point

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import app from './app.js';
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 
@@ -11,19 +11,19 @@ app.use(morgan('tiny'));
 app.use(express.json());
 
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 app.use((err, req, res, next) => {
-  console.log(err.stack);
-  if (!err.status) {
-    console.log(err.stack);
-    err.status = 500;
-    err.message = 'Internal Server Error';
-  }
-  res.status(err.status).json({ error: err.message });
+	console.log(err.stack);
+	if (!err.status) {
+		console.log(err.stack);
+		err.status = 500;
+		err.message = 'Internal Server Error';
+	}
+	res.status(err.status).json({ error: err.message });
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
